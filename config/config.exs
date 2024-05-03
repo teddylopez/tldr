37a053791e2741ca65d0cbf030c95819dd2edfba
Ted Lopez <tlopez@raysbaseball.com>
2024-05-03 17:11:03 -0400

    Enable liveview browser console debugging

--- config/config.exs
+++ config/config.exs
@@ -64,3 +64,5 @@ config :phoenix, :json_library, Jason
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
+
+config :phoenix_live_view, debug_heex_annotations: true
